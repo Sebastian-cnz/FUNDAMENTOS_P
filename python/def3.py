9fe912506279aa71fecf28b3bb1e1918fd8628c8
Sebastian-cnz <127146637+Sebastian-cnz@users.noreply.github.com>
2023-05-18 00:03:29 -0500

    Add files via upload

---
+++ python/def3.py
@@ -0,0 +1,12 @@
+def mostrar_mensaje (mensaje):
+    print(mensaje)
+
+def carga_suma ():
+    valor=int (input("ingrese el primer valor: "))
+    valor2=int (input("ingrese el segundo valor: "))
+    suma=valor + valor2
+    print ("la suma de los valores es: ",suma)
+
+mostrar_mensaje ("el programa calcula la suma de dos valores ingrsados por teclodo: ")
+carga_suma ()
+mostrar_mensaje ("gracias por utilizar el programa <3")
